5284bde38ee6b50a56be463a8383683789430e18
A&D Coding <82700860+adcodingacademy@users.noreply.github.com>
2021-04-19 10:30:46 +0200

    Create përparësia_e_veprimeve.py

---
+++ përparësia_e_veprimeve.py
@@ -0,0 +1,16 @@
+# Përparësia e operatorëve në Python
+
+# Python ndjek rregullat aritmetike për rradhën e veprimeve të operatorëve.
+
+# Për të ndryshuar këtë ne mund të përdorim shprehjet në kllapa
+
+# Shembull:
+
+print( 3 + 2 * 2)
+# kjo afishon vlerën 7 sepse kryhet shumëzimi pastaj mbledhja
+
+# por
+print(( 3 + 2) * 2)
+# afishon vlerën 10 pasi kryhen në fillim veprimet në kllapa d.m.th:
+# llogaritet në fillim: 3 + 2 = 5
+# më pas: 5 * 2 = 10 
