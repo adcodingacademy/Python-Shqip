71da33c56e704beec03b7b81d7a86ef0949f2d23
A&D Coding <82700860+adcodingacademy@users.noreply.github.com>
2021-04-19 07:45:47 +0200

    Create cikli_for.py

---
+++ cikli_for.py
@@ -0,0 +1,17 @@
+# ciklet = automatizim i detyrave të caktuara
+# cikli for = për të kaluar në çdo element të një sekuence
+
+# deklarimi i listës:
+emri = ["Armand", "Alfred", "Ardian"]
+mbiemri = ["Marku", "Deda" , "Paloka"]
+
+# deklarimi i cikilit for:
+for nxënës in emri:
+# për çdo nxënës në klasë:
+# i tregon Python që të nxjerrë një emër nga lista klasa
+# dhe t'a ruajë atë si vlerë të ndryshores nxënës
+
+# : sinjalizojnë nisjen e një blloku kodi
+# çdo kod që ekzekutohet brenda një blloku duhet të vendoset në kryeradhë
+    print(nxënës)
+# brenda ciklit afishojmë vlerën e nryshores nxënës
