a8d00752b10fd1ba1ea1b5b2a76422578a80b022
A&D Coding <82700860+adcodingacademy@users.noreply.github.com>
2021-04-19 10:43:15 +0200

    Create operatorët_e_krahasimit.py

---
+++ operatorët_e_krahasimit.py
@@ -0,0 +1,21 @@
+# Operatorët e krahasimit:
+#  1. < : më e vogël se
+#  2. > : më e madhe se
+#  3. <= : më e vogël se ose baraz me
+#  4. >= : më e madhe se ose baraz me 
+#  5. == : i ngjashëm (ose barabartë) me
+#  6. != : jo i (ose jo i barabartë) ngjashëm me
+
+# Shprehje boolean sepse jep një vlerë boolean
+x = 3 < 2
+print(x)
+x = 3 > 2
+print(x)
+x = 3 <= 2
+print(x)
+x = 3 >= 2
+print(x)
+x = "Përshëndetje" == "Përshëndetje"
+print(x)
+x = "Përshëndetje" != "përshëndetje"
+print(x)
